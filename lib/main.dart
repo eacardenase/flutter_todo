7b9d3b0b50831078e250c43b2a51e27578360bd8
Edwin Cardenas <eacardenase@unal.edu.co>
2024-01-18 09:09:57 -0500

    feat: columns and expanded widgets

--- lib/main.dart
+++ lib/main.dart
@@ -12,73 +12,25 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: const Text(
-            "Flutter Lover",
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-          backgroundColor: Colors.cyan.shade300,
-          elevation: 0,
-          leading: const Icon(
-            Icons.menu,
-            color: Colors.white,
-          ),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.logout,
-                color: Colors.white,
+        body: Column(
+          children: [
+            Expanded(
+              child: Container(
+                color: Colors.deepPurple,
               ),
-            )
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                color: Colors.deepPurple.shade400,
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.deepPurple.shade300,
+              ),
+            ),
           ],
-        ),
-        body: Center(
-          child: Container(
-            height: 300,
-            width: 300,
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.all(
-                Radius.circular(
-                  20,
-                ),
-              ),
-              color: Colors.deepPurple,
-            ),
-            padding: const EdgeInsets.all(
-              10,
-            ),
-            child: const Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Edwin",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 26,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Icon(
-                    Icons.favorite,
-                    color: Colors.pink,
-                  ),
-                  Text(
-                    "Flutter",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 26,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
         ),
       ),
       debugShowCheckedModeBanner: false,
